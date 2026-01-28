ede5ab2b852a303fbdd4b71d01bbfb74b4947ae8
Tasi Szabina <tasi.szabina28@gmail.com>
2026-01-28 14:24:17 +0100

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    {/* 2. TEDD BE IDE A KOMPONENST: */}
+    <Analytics />
+  </React.StrictMode>,
+)
 
 createRoot(document.getElementById("root")!).render(<App />);
